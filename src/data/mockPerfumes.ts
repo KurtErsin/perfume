d3a6b30c537087bdb8960d9d549de1eec7661414
KurtErsin <ersin.e.kurt@gmail.com>
2025-12-03 12:12:53 +1100

    Update mockPerfumes.ts
    eski hali

--- src/data/mockPerfumes.ts
+++ src/data/mockPerfumes.ts
@@ -13043,15 +13043,6 @@ const unsortedPerfumes: Perfume[] = [
         slug: 'creed-eladaria', 
         isNew: true,
       },
-         {
-        id: '1719',
-        name: 'Inspired by ERSIN - EREM (W821)', 
-        brand: 'EREM', 
-        gender: 'female',
-        notes: ['rose', 'musky', 'powdery', 'floral', 'amber'],
-        slug: 'creed-eladaria', 
-        isNew: true,
-      },
   ];
 
 
